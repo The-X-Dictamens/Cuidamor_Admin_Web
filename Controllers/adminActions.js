9873fac36a1f0af6fde8c1821d66dee2932c19be
JZJPc9 <99989355+JZJPc9@users.noreply.github.com>
2024-05-01 13:04:03 -0600

    agregar empleado incompleto
    pruebas unitarias del modulo

--- Controllers/adminActions.js
+++ Controllers/adminActions.js
@@ -51,17 +51,19 @@ exports.getSolicitudProceso = async (req, res) => {
 
 exports.crearEmpleado = async (req, res) => {
     if (req.session.user) {
-        console.log(req.body);
-        console.log(req.files);
-        
         try {
+            //obtencion de los datos del formulario
             let { nombre, apellido_paterno, apellido_materno, correo, numero_telefono, calle, colonia, codigo_postal, alcaldia, cedula_profecional, puesto} = req.body;
+            console.log(nombre, apellido_paterno, apellido_materno, correo, numero_telefono, calle, colonia, codigo_postal, alcaldia, cedula_profecional, puesto);
+            /*
             let dataAcces = await query("INSERT INTO datos_acceso (corr_datacc, pass_datacc, rol_datacc) VALUES (?,?,?)", [correo, password, puesto]);
-            let idDataAcces = dataAcceso.insertId;
+            let idDataAcces = dataAcces.insertId;
             let dataDireccion = await query("INSERT INTO direccion (del_dir, col_dir, calle_dir, cp_dir) VALUES (?,?,?,?)",[alcaldia, colonia, calle, codigo_postal])
             let idDataDirec = dataDireccion.insertId;
-            let empleado = await query("INSERT INTO empleado (nom_emp, pat_emp, mat_emp, tel_emp, id_datacc) VALUES (?,?,?,?,?)", [nombre, paterno, materno, telefono]);
+            let empleado = await query("INSERT INTO empleado (nom_emp, pat_emp, mat_emp, fot_emp, tel_emp, est_emp, id_datacc, id_dir) VALUES (?,?,?,'N/A',?,'Proceso',?,?)", [nombre, apellido_paterno, apellido_materno, numero_telefono, idDataAcces, idDataDirec]);
+            let idEmpleado = empleado.insertId;
             res.redirect("/Empleadosenproceso");
+            */
         } catch (err) {
             console.error(err);
         }
