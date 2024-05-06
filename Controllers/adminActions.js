ef1c17740ad0f62c64469f8a7b6f4a565c949687
JZJPc9 <99989355+JZJPc9@users.noreply.github.com>
2024-05-06 17:57:02 -0600

    comit
    configuracion

--- Controllers/adminActions.js
+++ Controllers/adminActions.js
@@ -3,6 +3,7 @@ const { promisify } = require("util");
 const query = promisify(coneccion.query).bind(coneccion);
 const cloudController = require("./cloudController");
 const gmailController = require("./gmailcontroller");
+const e = require("express");
 
 exports.login = async (req, res) => {
   try {
@@ -57,6 +58,21 @@ exports.getSolicitudAceptada = async (req, res) => {
         "SELECT e.id_emp, e.nom_emp, e.pat_emp, e.mat_emp, e.fot_emp, e.tel_emp, e.est_emp, da.* FROM empleado e JOIN datos_acceso da ON e.id_datacc = da.id_datacc;"
       );
       res.render("Emple-acep", { empleados: empleados });
+    } catch (err) {
+        console.error(err)
+    }
+  } else {
+    res.redirect("/");
+  }
+};
+
+exports.getSolicitudRechazado = async (req, res) => {
+  if (req.session.user) {
+    try {
+      const empleados = await query(
+        "SELECT e.id_emp, e.nom_emp, e.pat_emp, e.mat_emp, e.fot_emp, e.tel_emp, e.est_emp, da.* FROM empleado e JOIN datos_acceso da ON e.id_datacc = da.id_datacc;"
+      );
+      res.render("Emple-rech", { empleados: empleados });
     } catch (err) {
         console.error(err)
     }
@@ -121,6 +137,7 @@ exports.verEmpleado = async (req, res) => {
             let id = req.query.id;
             let empleado = await query("SELECT e.id_emp, e.nom_emp, e.pat_emp, e.mat_emp, e.fot_emp, e.tel_emp, e.est_emp, da.*, d.* FROM empleado e JOIN datos_acceso da ON e.id_datacc = da.id_datacc JOIN direccion d ON e.id_dir = d.id_dir WHERE e.id_emp = ?", [id]);
             let perfil = await query("SELECT * FROM perfil_profecional WHERE id_emp = ?", [id]);
+            console.log(perfil);
             console.log(empleado);
 
             //urls de las fotos de los documenstos
@@ -145,4 +162,156 @@ exports.verEmpleado = async (req, res) => {
 }
 
 
+
+
+exports.eliminarDocumento = async (req, res) => {
+
+  if(req.session.user){
+
+    let iduser = req.query.id;
+    let archivo = req.query.doc;
+
+    console.log(iduser,archivo);
+
+    try {
+      let perfil = await query("SELECT * FROM perfil_profecional WHERE id_emp = ?", [iduser]);
+      switch (archivo) {
+        case "comprobante":
+          await cloudController.delete(perfil[0].comdom_prof);
+          await query("UPDATE perfil_profecional SET comdom_prof = null WHERE id_emp = ?", [iduser]);
+          break;
+        case "ine":
+          await cloudController.delete(perfil[0].ine_prof);
+          await query("UPDATE perfil_profecional SET ine_prof = null WHERE id_emp = ?", [iduser]);
+          break;
+        case "certificados":
+          await cloudController.delete(perfil[0].cert_prof);
+          await query("UPDATE perfil_profecional SET cert_prof = null WHERE id_emp = ?", [iduser]);
+          break;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    
+    res.redirect("/Empleado?id="+iduser);
+
+  }else{
+    res.redirect("/")
+  }
+
+}
+
+
+
+
+//subir documento a expediente de empleado
+exports.subirDocumento = async (req, res) => {
+  if (req.session.user) {
+    try {
+      let id = req.query.id;
+      let tipo = req.query.tipo;
+      let documento = req.file;
+      console.log(id, tipo, documento);
+
+      try{
+        let name = documento.fieldname + "-" + id + "." + documento.originalname.split(".").pop();
+        switch (tipo) {
+          case "comprobante":
+            await query("UPDATE perfil_profecional SET comdom_prof = ? WHERE id_emp = ?", [name, id]);
+            await cloudController.upload(documento, name);
+            break;
+          case "ine":
+            await query("UPDATE perfil_profecional SET ine_prof = ? WHERE id_emp = ?", [name, id]);
+            await cloudController.upload(documento, name);
+            break;
+          case "certificados":
+            await query("UPDATE perfil_profecional SET cert_prof = ? WHERE id_emp = ?", [name, id]);
+            await cloudController.upload(documento, name);
+            break;
+        }
+        res.redirect("/Empleado?id="+id);
+
+
+      }catch(err){
+        console.error(err);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    res.redirect("/");
+  }
+};
+
+
+
+
+
+exports.rechazarSolicitud = async (req, res) => {
+  if (req.session.user) {
+    try {
+      let id = req.query.id;
+      await query("UPDATE empleado SET est_emp = 'Rechazado' WHERE id_emp = ?", [id]);
+      res.redirect("/Empleadosenproceso");
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    res.redirect("/");
+  }
+};
+
+exports.aceptarSolicitud = async (req, res) => {
+  if (req.session.user) {
+    try {
+      let id = req.query.id;
+      await query("UPDATE empleado SET est_emp = 'Aceptado' WHERE id_emp = ?", [id]);
+
+      /*
+      //veo si la contraseña es null o ya tiene una
+      let data = await query("SELECT * FROM datos_acceso WHERE id_datacc = ?", [id]);
+      if (data[0].pass_datacc == null) {
+        //si no tiene contraseña se le envia un correo para que la genere
+        let correo = data[0].cor_datacc;
+
+        let pass = 
+
+        let token = await gmailController.sendEmail(correo,`Cuidamos Datos para acceder a tu correo",<h1>correo:  ${correo}</h1><br><h1>contraseña: ${password}`);
+        await query("UPDATE datos_acceso SET pas_datacc = ? WHERE id_datacc = ?", [token, id]);
+      }
+      */
+
+      res.redirect("/Empleadosaceptados");
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    res.redirect("/");
+  }
+}
+
+
+exports.procesarSolicitud = async (req, res) => {
+  if (req.session.user) {
+    try {
+      let id = req.query.id;
+      await query("UPDATE empleado SET est_emp = 'Proceso' WHERE id_emp = ?", [id]);
+      res.redirect("/Empleadosenproceso");
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    res.redirect("/");
+  }
+}
+
+
+
+
+
+
+
+
+
+//generar una contraseña pseudo aleatoria
 
--- routes/router.js
+++ routes/router.js
@@ -18,20 +18,38 @@ router.get('/Empleadosenproceso',adminActions.getSolicitudProceso);
 //ruta de empleados apceptados
 router.get('/Empleadosaceptados',adminActions.getSolicitudAceptada);
 
+//ruta de empleados apceptados
+router.get('/Empleadosrechazado',adminActions.getSolicitudRechazado);
+
 //captura de de informacion del formulario de agregar
 router.post('/crearEmpleado',upload.fields([{name: 'comprobante_domicilio'},{name: 'ine'},{name:'certificados'}]),adminActions.crearEmpleado);
 
 //visualizar la informacion del empleado
 router.get('/Empleado',adminActions.verEmpleado);
 
+//eliminar solo un documento de empleado 
+router.get('/EliminarDoc',adminActions.eliminarDocumento);
+
+//agregar documento
+router.post('/SubirDoc',upload.single('documento'),adminActions.subirDocumento);
+
+//Rechazar solicitud
+router.get('/RechazarEmpleado',adminActions.rechazarSolicitud);
+
+//aceptar solicitud
+router.get('/AceptarEmpleado',adminActions.aceptarSolicitud);
+
+//procesar solicitud
+router.get('/ProcesarEmpleado',adminActions.procesarSolicitud);
 
 
 
-
+//chachado de error 404
 router.use(function(req, res, next) {
     res.status(404).render('catchError', {message:'Página no encontrada'});
 });
 
+//manejo de errores
 router.use(function(err, req, res, next){
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,10 +57,6 @@ router.use(function(err, req, res, next){
     res.send(err.message);
     console.log(err);
 });
-
-
-
-
 
 
 
