309eb38b65ebdcbc18dd6fcf79dae03e2731d5a5
JZJPc9 <99989355+JZJPc9@users.noreply.github.com>
2024-05-23 17:41:50 -0600

    Front-end finish PPS
    Finalizado el dar estilo a las pruebas psicometricas

--- routes/router.js
+++ routes/router.js
@@ -67,7 +67,7 @@ router.use(function(err, req, res, next){
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500);
-    res.send(err.message);
+    res.send('Hubo algun error con los servicios por favor intente mas tarde');
     console.log(err);
 });
 
